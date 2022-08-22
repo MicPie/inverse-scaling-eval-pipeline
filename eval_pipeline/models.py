74e40393b7021e1f5ee5ae03c622940995f3fbe7
Michael Pieler <Michael.Pieler@Gmail.com>
2022-08-22 19:58:02 +0200

    fix opt tokenizer with use_fast=False based on model name

--- eval_pipeline/models.py
+++ eval_pipeline/models.py
@@ -106,7 +106,7 @@ class HFModel(Model):
                     ).to(self.device)  # type: ignore
         # apparently the OPT models need slightly different tokenizers
         # https://huggingface.co/docs/transformers/main/en/model_doc/opt#overview
-        if prefix == "opt":
+        if model_name.startswith("opt-"):
             use_fast = False
         else:
             use_fast = True
